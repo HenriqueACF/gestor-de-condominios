65aaec670272b56399f03030f59a066060b2f438
Henrique Assis <henrique_assis_10@hotmail.com>
2021-02-04 14:39:20 -0300

    init custondrawer

---
+++ devcond/src/components/DrawerCustom.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+import { useStateValue } from '../contexts/StateContext';
+
+const DrawerArea = styled.View `
+    flex:1;
+    background-color:#FF0000;
+`;
+
+const DrawerLogoArea = styled.View ``;
+const DrawerLogo = styled.Image ``;
+const DrawerScroller = styled.ScrollView ``;
+const ChangeUnitArea = styled.View ``;
+const ChangeUnitButton = styled.TouchableOpacity ``;
+const ChangeUnitButtonText = styled.Text ``;
+const FooterArea = styled.View ``;
+const FooterInfo = styled.View ``;
+const FooterProfile = styled.Text ``;
+const FooterUnitText = styled.Text ``;
+const FooterUnitButton = styled.TouchableOpacity ``;
+
+export default (props) =>{
+
+    const navigation = useNavigation();
+    const [context, dispatch] = useStateValue();
+
+    return(
+        <DrawerArea>
+            <DrawerLogoArea>
+                <DrawerLogo source={require('../assets/homelogo.png')} resizeMode="contain"/>
+            </DrawerLogoArea>
+
+            <DrawerScroller>
+
+            </DrawerScroller>
+
+            <ChangeUnitArea>
+                <ChangeUnitButton>
+                    <ChangeUnitButtonText>
+                        Trocar Unidade
+                    </ChangeUnitButtonText>
+                </ChangeUnitButton>
+            </ChangeUnitArea>
+
+            <FooterArea>
+                <FooterInfo>
+                    <FooterProfile>Teste</FooterProfile>
+                    <FooterUnitText>Nome da Propriedade</FooterUnitText>
+                </FooterInfo>
+                <FooterUnitButton>
+                    <Icon name='gear' size={24} color="#666e78" />
+                </FooterUnitButton>
+            </FooterArea>
+        </DrawerArea>
+    );
+}
