c102827ba0acd23f879c006c20106f77cc1d13b2
Henrique Assis <henrique_assis_10@hotmail.com>
2021-02-09 15:51:47 -0300

    documentScreen

---
+++ devcond/src/screens/DocumentScreen/style.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components/native';
+
+export default{
+    Container: styled.SafeAreaView `
+        flex:1;
+        padding:20px;
+        background-color:#f5f6fa;
+    `,
+
+    LoadingIcon: styled.ActivityIndicator ``,
+
+    NoListArea: styled.View `
+        justify-content:center;
+        align-items:center;
+        padding:30px;
+    `,
+
+    NoListText: styled.Text `
+        font-size:15px;
+        color:#000;
+    `,
+
+    List: styled.FlatList `
+    flex:1;
+    `,
+        
+};
