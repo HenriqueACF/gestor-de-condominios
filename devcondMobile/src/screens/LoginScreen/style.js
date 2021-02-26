a6820eba2a80012fb9b9dd92ea9fce9f1b1452b4
HenriqueACF <henrique_assis_10@hotmail.com>
2021-02-26 17:38:44 -0300

    init loginScreen

--- devcondMobile/src/screens/LoginScreen/style.js
+++ devcondMobile/src/screens/LoginScreen/style.js
@@ -3,11 +3,41 @@ import styled from 'styled-components/native';
 export default{
     Container: styled.SafeAreaView `
         flex:1;
-        justify-content:center;
-        align-items:center;
+        padding:20px;
+        background-color:#F5F6FA
     `,
     
-    Texto: styled.Text `
-    
-    `
+    Logo:styled.Image `
+        width:250px;
+        height:200px;
+        margin-left:auto;
+        margin-right:auto;
+        margin-bottom:20px;
+    `,
+
+    Field: styled.TextInput `
+        border-width:1px;
+        border-color:#CCC;
+        background-color:#FFF;
+        border-radius:5px;
+        color:#000;
+        font-size:15px;
+        padding:10px;
+        margin-bottom:15px;
+    `,
+
+    ButtonArea:styled.TouchableOpacity `
+        background-color:#8863E6;
+        padding:12px;
+        justify-content:center;
+        align-items:center;
+        border-radius:5px;
+        margin-bottom:15px;
+    `,
+
+    ButtonAreaText:styled.Text `
+        color:#FFF;
+        font-size:15px;
+        font-weight:bold;
+    `,
 }
