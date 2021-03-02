6af65d9cda7a9a7e5e73e2b2fc82670283c48c1e
HenriqueACF <henrique_assis_10@hotmail.com>
2021-03-02 09:12:00 -0300

    header title

--- devcondMobile/src/stacks/AuthStack.js
+++ devcondMobile/src/stacks/AuthStack.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
+//Telas
 import PreloadScreen from '../screens/PreloadScreen';
 import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
 
 const Stack = createStackNavigator();
 
 export default () =>{
     return(
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{
+            headerStyle:{
+                backgroundColor:'#F5F6FA',
+                elevation:0,
+                shadowOpacity:0,
+            }
+        }}>
             <Stack.Screen 
                 name="PreloadScreen"
                 component={PreloadScreen}
@@ -19,6 +27,12 @@ export default () =>{
                 name="LoginScreen"
                 component={LoginScreen}
                 options={{headerShown:false}}
+            />
+
+            <Stack.Screen 
+                name="RegisterScreen"
+                component={RegisterScreen}
+                options={{headerShown:true}}
             />
         </Stack.Navigator>
     )
