323590485bf3d3ae6dc282ff38388470cbdad351
HenriqueACF <henrique_assis_10@hotmail.com>
2021-03-03 08:42:22 -0300

    init Drawer

---
+++ devcondMobile/src/screens/WallScreen/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export default{
+    Container: styled.SafeAreaView `
+        flex:1;
+        padding:20px;
+        background-color:#F5F6FA
+    `,
+    
+    Scroller: styled.ScrollView `
+        flex:1;
+        padding:20px;
+    `,
+}
