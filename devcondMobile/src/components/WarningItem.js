77b786c93106fbaee5a8144e02edab28b90c40d6
HenriqueACF <henrique_assis_10@hotmail.com>
2021-03-09 23:15:41 -0300

    style WarningItem

--- devcondMobile/src/components/WarningItem.js
+++ devcondMobile/src/components/WarningItem.js
@@ -5,36 +5,77 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 
 import api from '../services/api';
 
-const Box = styled.TouchableOpacity `
+const Box = styled.View `
     background-color:#FFF;
     border-width:2px;
     border-color:#E8E9ED;
     border-radius:15px;
     padding:15px;
     margin-bottom:10px;
-    flex-direction:row;
-    align-items:center;
+`;
+
+const Date = styled.Text `
+    font-size:14px;
+    font-weight:bold;
+    color:#9C9DB9;
+    margin-bottom:10px;
 `;
 
 const Title = styled.Text `
     font-size:15px;
     color:#000;
+`;
+
+const StatusArea = styled.View `
+    flex-direction:row;
+    align-items:center;
+    margin:10px 0;
+`;
+
+const StatusText = styled.Text `
+    font-size:14px;
+    color:#9C9DB9;
     margin-left:10px;
+`;
+
+const PhotosArea = styled.View `
+    flex-direction:row;
+`;
+
+const PhotoItem = styled.TouchableOpacity `
+    margin-right:10px;
+`;
+
+const PhotoImage = styled.Image `
+    width:50px;
+    height:50px;
+    border-radius:10px;
 `;
 
 export default({data})=>{
 
-    const handleClick = async() =>{
-        const supported = await Linking.canOpenURL(data.fileurl);
-        if(supported){
-            await Linking.openURL(data.fileurl);
-        }
-    }
 
     return(
-        <Box onPress={handleClick}>
-            <Icon name="file-text" size={30} color="#8B63E7" />
+        <Box >
+            <Date>{data.datecreated}</Date>
             <Title>{data.title}</Title>
+            <StatusArea>
+                <Icon name="inbox" size={24} color="#8B63E7" />
+                <StatusText>
+                    {data.status === 'IN_REVIEW' && 'Ocorrência em análise'} 
+                    {data.status === 'RESOLVED' && 'Ocorrência resolvida'} 
+                </StatusText>
+            </StatusArea>
+
+            {data.photos.length > 0 && 
+                <PhotosArea>
+                    {data.photos.map((item, index)=>(
+                        <PhotoItem key={index} onPress={null}>
+                            <PhotoImage source={{uri:item}} resizeMode="cover" />
+                        </PhotoItem>
+                    ))}
+                </PhotosArea>
+            }
         </Box>
     );
 }
