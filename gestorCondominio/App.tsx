25fec9a6a8e1d4b789ceb0edfbea59ee9639eb03
HenriqueACF <henrique.assis145@gmail.com>
2022-08-31 22:29:12 -0300

    recriando projeto com expo, e configurando novamente

--- gestorCondominio/App.tsx
+++ gestorCondominio/App.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
-import {StateProvider} from './src/contexts/StateContext';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default () => {
+export default function App() {
   return (
-    <StateProvider>
-      <SafeAreaView>
-        <Text>Oi</Text>
-      </SafeAreaView>
-    </StateProvider>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
